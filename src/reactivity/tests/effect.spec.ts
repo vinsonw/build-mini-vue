b51785a1917184f6904264fb1438ec76ad9de0a1
vinsonw <me@vinsonwei.com>
2024-01-21 18:25:00 +0800

    feat: add scheduler option for effect()

--- src/reactivity/tests/effect.spec.ts
+++ src/reactivity/tests/effect.spec.ts
@@ -30,4 +30,50 @@ describe("effect", () => {
     expect(res).toBe("foo")
     expect(foo).toBe(12)
   })
+
+  it("scheduler", () => {
+    let dummy
+    let run: any
+    const scheduler = jest.fn(() => {
+      run = runner
+    })
+    const obj = reactive({ foo: 1 })
+    // now effect() accepts 2 params, (fn, options?: {scheduler})
+    const runner = effect(
+      () => {
+        dummy = obj.foo
+      },
+      { scheduler }
+    )
+
+    // scheduler should not be called yet
+    expect(scheduler).not.toHaveBeenCalled()
+    expect(dummy).toBe(1)
+    // trigger setter
+    obj.foo++
+    // scheduler now should be called once
+    expect(scheduler).toHaveBeenCalledTimes(1)
+    // but scheduler didn't call fn, so dummy is not changed
+    expect(dummy).toBe(1)
+    // fn is called
+    run()
+    // now dummy is changed
+    expect(dummy).toBe(2)
+  })
+
+  it.skip("stop", () => {
+    let dummy
+    const obj = reactive({ prop: 1 })
+    const runner = effect(() => {
+      dummy = obj.prop
+    })
+    obj.prop = 2
+    expect(dummy).toBe(2)
+    stop(runner)
+    obj.prop = 3
+    expect(dummy).toBe(2)
+
+    runner()
+    expect(dummy).toBe(3)
+  })
 })
