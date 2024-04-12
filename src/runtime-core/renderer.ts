6e0cd15f253568121a83b1a683c46a03da3535f2
vinsonw <me@vinsonwei.com>
2024-04-12 23:06:59 +0800

    chore: move getSequence function to separate file

--- src/runtime-core/renderer.ts
+++ src/runtime-core/renderer.ts
@@ -4,6 +4,7 @@ import { Fragment, Text } from "./vnode"
 import { createAppAPI } from "./createApp"
 import { effect } from "src/reactivity/effect"
 import { EMPTY_OBJ } from "src/shared"
+import { getSequence } from "./algo"
 
 export function createRenderer(options: {
   createElement: (type: string) => any
@@ -341,47 +342,6 @@ export function createRenderer(options: {
         }
       }
     }
-  }
-
-  function getSequence(arr) {
-    const p = arr.slice()
-    const result = [0]
-    let i, j, u, v, c
-    const len = arr.length
-    for (i = 0; i < len; i++) {
-      const arrI = arr[i]
-      if (arrI !== 0) {
-        j = result[result.length - 1]
-        if (arr[j] < arrI) {
-          p[i] = j
-          result.push(i)
-          continue
-        }
-        u = 0
-        v = result.length - 1
-        while (u < v) {
-          c = (u + v) >> 1
-          if (arr[result[c]] < arrI) {
-            u = c + 1
-          } else {
-            v = c
-          }
-        }
-        if (arrI < arr[result[u]]) {
-          if (u > 0) {
-            p[i] = result[u - 1]
-          }
-          result[u] = i
-        }
-      }
-    }
-    u = result.length
-    v = result[u - 1]
-    while (u-- > 0) {
-      result[u] = v
-      v = p[v]
-    }
-    return result
   }
 
   function isSameVNodeType(n1, n2) {
